851f8ee997c4570d43e47063686798026bacb43b
MinhNDMI <110145202+MinhNDMI@users.noreply.github.com>
2024-08-06 15:56:26 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { Layout, Menu, Card, Select, Checkbox, Button, Row, Col } from "antd";
-import { DownOutlined } from "@ant-design/icons";
+import "./App.css";
+import {
+  Layout,
+  Menu,
+  Card,
+  Select,
+  Checkbox,
+  Button,
+  Row,
+  Col,
+  Drawer,
+} from "antd";
+import { DownOutlined, UpOutlined, MenuOutlined } from "@ant-design/icons";
 import productsData from "./data/products.json";
 import storesData from "./data/stores.json";
 import storeProductsData from "./data/storeProducts.json";
-import "./App.css";
 
 const { Header, Sider, Content } = Layout;
 const { Option } = Select;
@@ -15,6 +25,15 @@ const App = () => {
   const [sortKey, setSortKey] = useState("name");
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [filterVisible, setFilterVisible] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     const getFilteredProducts = () => {
@@ -29,7 +48,11 @@ const App = () => {
       if (toppingFilters.length > 0) {
         filteredProducts = filteredProducts.filter((product) =>
           toppingFilters.every((topping) =>
-            product.toppings.toLowerCase().split(',').map(t => t.trim()).includes(topping.toLowerCase())
+            product.toppings
+              .toLowerCase()
+              .split(",")
+              .map((t) => t.trim())
+              .includes(topping.toLowerCase())
           )
         );
       }
@@ -59,14 +82,7 @@ const App = () => {
 
   const handleStoreChange = (store) => {
     setSelectedStore(store);
-  };
-
-  const handleToppingChange = (topping) => {
-    setToppingFilters((prevFilters) =>
-      prevFilters.includes(topping)
-        ? prevFilters.filter((t) => t !== topping)
-        : [...prevFilters, topping]
-    );
+    onClose()
   };
 
   const handleSortChange = (value) => {
@@ -75,67 +91,67 @@ const App = () => {
 
   const toggleFilter = () => {
     setFilterVisible(!filterVisible);
-    setToppingFilters([])
+    setToppingFilters([]);
   };
 
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Sider width={200} className="site-layout-background">
+    <div className="container" style={{ minHeight: "100vh" }}>
+      <Sider width={200} className="sider-menu">
         <Menu
           mode="inline"
           defaultSelectedKeys={[`${selectedStore.id}`]}
           style={{ height: "100%", borderRight: 0 }}
         >
+          <h3 style={{textAlign:'center', padding:'24px'}}>Milk Tea Store</h3>
           {storesData.map((store) => (
-            <Menu.Item
-              key={store.id}
-              onClick={() => handleStoreChange(store)}
-            >
-              {store.name}
+            <Menu.Item key={store.id} onClick={() => handleStoreChange(store)}>
+              <p style={{textAlign:'center'}}>{store.name}</p>
             </Menu.Item>
           ))}
         </Menu>
       </Sider>
       <Layout style={{ padding: "0 24px 24px" }}>
         <Header>
-          <h1 style={{ margin: "5px", textAlign:'center'}}>{selectedStore.name} Menu</h1>
+          <div style={{ display: "flex", justifyContent: "center"}}>
+            <h1
+              style={{
+                margin: "5px",
+              }}
+            >
+              {selectedStore.name} Menu
+            </h1>
+            <MenuOutlined className="menu-store" style={{position:'absolute', top:'15px', right:'15px'}} onClick={showDrawer}/>
+          </div>
         </Header>
-        
+
         <Content
           className="site-layout-background"
           style={{
-            padding: 24,
-            margin: 0,
             minHeight: 280,
           }}
         >
           <Row justify="space-between">
-            <Col>
-              {/* <Button onClick={() => setToppingFilters([])}>Filter</Button> */}
+            <Col style={{ display: "flex", alignItems: "center", gap: 4 }}>
               <Button onClick={toggleFilter}>
                 Filter{" "}
-                {filterVisible ? (
-                  <DownOutlined />
-                ) : (
-                  <DownOutlined rotate={180} />
-                )}
+                {filterVisible ? <UpOutlined /> : <UpOutlined rotate={180} />}
               </Button>
             </Col>
             <Col>
-              <div style={{display: 'flex', alignItems: 'center', gap: 4}}>
+              <div style={{ display: "flex", alignItems: "center", gap: 4 }}>
                 <p>Sort by</p>
-              <Select
-                defaultValue="name"
-                style={{ width: 120 }}
-                onChange={handleSortChange}
-                dropdownMatchSelectWidth={false}
-                suffixIcon={<DownOutlined />}
-              >
-                <Option value="name">Name (Asc)</Option>
-                <Option value="name-desc">Name (Dsc)</Option>
-                <Option value="price">Price (Asc)</Option>
-                <Option value="price-desc">Price (Dsc)</Option>
-              </Select>
+                <Select
+                  defaultValue="name"
+                  style={{ width: 120 }}
+                  onChange={handleSortChange}
+                  dropdownMatchSelectWidth={false}
+                  suffixIcon={<DownOutlined />}
+                >
+                  <Option value="name">Name (Asc)</Option>
+                  <Option value="name-desc">Name (Dsc)</Option>
+                  <Option value="price">Price (Asc)</Option>
+                  <Option value="price-desc">Price (Dsc)</Option>
+                </Select>
               </div>
             </Col>
           </Row>
@@ -157,19 +173,39 @@ const App = () => {
           )}
           <Row gutter={16}>
             {filteredProducts.map((product) => (
-              <Col span={8} key={product.id} style={{ marginBottom: 16 }}>
+              <Col
+                key={product.id}
+                xs={{ span: 24 }} // 1 cột trên màn hình nhỏ hơn 576px
+                sm={{ span: 12 }} // 2 cột trên màn hình từ 576px đến 767px
+                md={{ span: 8 }} // 3 cột trên màn hình từ 768px đến 991px
+                style={{ marginBottom: 16 }}
+              >
                 <Card
                   title={product.name}
-                  extra={<span>${product.price.toFixed(2)}</span>}
+                  // extra={<span>${product.price.toFixed(2)}</span>}
                 >
                   <p>Toppings: {product.toppings}</p>
+                  <p style={{textAlign: 'end'}}>${product.price.toFixed(2)}</p>
                 </Card>
               </Col>
             ))}
           </Row>
         </Content>
       </Layout>
-    </Layout>
+      <Drawer title="Milk Tea Store" onClose={onClose} open={open}>
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={[`${selectedStore.id}`]}
+          style={{ height: "100%", borderRight: 0 }}
+        >
+          {storesData.map((store) => (
+            <Menu.Item key={store.id} onClick={() => handleStoreChange(store)}>
+              {store.name}
+            </Menu.Item>
+          ))}
+        </Menu>
+      </Drawer>
+    </div>
   );
 };
 
